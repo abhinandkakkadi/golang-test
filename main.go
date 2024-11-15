7e75fd1554a8c8c58559da8b65ba8d5c5a7965e1
Abhinand KR <nanduttanvsabhi@gmail.com>
2024-11-15 11:39:24 +0530

    add test cases

--- main.go
+++ main.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println(add(1, 3))
+	fmt.Println(sub(3, 1))
 }
 
 func add(a, b int) int {
 	return a + b
 }
+
+func sub(a, b int) int {
+	return a - b
+}
+
--- main_test.go
+++ main_test.go
@@ -8,4 +8,10 @@ func Test_add(t *testing.T) {
 	if c != 3 {
 		t.Error("got error")
 	}
+
+	c := add(2, 1)
+
+	if c != 1 {
+		t.Error("got error")
+	}
 }
